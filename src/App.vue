1b00a6110d7b2ed04c077b132973bdbfb207577b
Ivan Trutnev <i@gmail.com>
2018-03-30 15:15:22 +0300

    add show/hide button for left navigation menu

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,57 @@
 <template>
-  <div class="container-fluid">
-    <div v-if="$route.path !== '/login'">
-    <header class="d-flex justify-content-end">
-      <button class="btn btn-secondary" @click="logout">Logout</button>
-    </header>
-      <div class="row">
-        <div class="col-sm-3 menu" v-if="this.$route.path !== '/login'">
-          <ul class="list-group">
-            <router-link v-for="(item, index) in menuList"
-                         :key="index"
-                         :to="item.url"
-                         tag="li"
-                         class="list-group-item"
-                         active-class="active"
-            >
-              <a>{{ item.text }}</a>
-            </router-link>
-          </ul>
-
+    <div class="container-fluid">
+        <div v-if="$route.path !== '/login'">
+            <header class="d-flex justify-content-end">
+                <button class="btn btn-secondary" @click="logout">Logout</button>
+            </header>
+            <div class="row">
+                <div class="menu" v-if="this.$route.path !== '/login'" v-bind:class="{ 'col-sm-3': showMenuFlag, 'col-sm-1': !showMenuFlag}">
+                    <div class="menu__btn">
+                        <button class="btn btn-info" @click="showMenuFlag = !showMenuFlag">
+                            <transition name="slide" mode="out-in">
+                                <span v-if="showMenuFlag">Hide</span>
+                                <span v-else>Show</span>
+                            </transition>
+                        </button>
+                    </div>
+                    <ul class="list-group" v-if="showMenuFlag">
+                        <router-link v-for="(item, index) in menuList"
+                                     :key="index"
+                                     :to="item.url"
+                                     tag="li"
+                                     class="list-group-item"
+                                     active-class="active"
+                        >
+                            <a>{{ item.text }}</a>
+                        </router-link>
+                    </ul>
+                </div>
+                <div v-bind:class="{ 'col-sm-9': showMenuFlag, 'col-sm-11': !showMenuFlag}">
+                    <transition name="slide" mode="out-in">
+                        <router-view></router-view>
+                    </transition>
+                </div>
+            </div>
         </div>
-        <div class="col-sm-9">
-          <transition name="slide" mode="out-in">
-            <router-view ></router-view>
-          </transition>
+        <div class="container" v-else>
+            <router-view></router-view>
         </div>
-      </div>
     </div>
-    <div class="container" v-if="$route.path === '/login'">
-      <router-view></router-view>
-    </div>
-  </div>
 </template>
 
 <script>
-  import {mapGetters} from 'vuex';
+  import { mapGetters } from 'vuex';
 
   export default {
+    data() {
+      return {
+        showMenuFlag: true
+      }
+    },
     methods: {
       logout() {
-          localStorage.removeItem('login');
-          location.reload();
+        localStorage.removeItem('login');
+        location.reload();
       }
     },
     computed: {
@@ -58,65 +70,70 @@
 </script>
 
 <style scoped>
-  .container-fluid {
-    padding-top: 15px;
-  }
-
-  .menu {
-    border-right: 1px solid #ddd;
-  }
-
-  .list-group-item {
-    transition: background 0.3s, color 0.3s;
-  }
-
-  .list-group-item a {
-    text-decoration: none;
-  }
-
-  .list-group-item.active a {
-    color: inherit;
-  }
-
-  .slide-enter {
-
-  }
-
-  .slide-enter-active {
-    animation: slideIn 0.5s;
-  }
-
-  .slide-enter-to {
-
-  }
-
-  .slide-leave {
-
-  }
-
-  .slide-leave-active {
-    animation: slideOut 0.5s;
-  }
-
-  .slide-leave-to {
-
-  }
-
-  @keyframes slideIn {
-    from {
-      transform: rotateY(90deg);
+    .container-fluid {
+        padding-top: 15px;
     }
-    to {
-      transform: rotateY(0deg);
-    }
-  }
 
-  @keyframes slideOut {
-    from {
-      transform: rotateY(0deg);
+    .menu {
+        /*border-right: 1px solid #ddd;*/
     }
-    to {
-      transform: rotateY(90deg);
+
+    .menu__btn, .list-group {
+        margin-top: 10px;
     }
-  }
+
+
+    .list-group-item {
+        transition: background 0.3s, color 0.3s;
+    }
+
+    .list-group-item a {
+        text-decoration: none;
+    }
+
+    .list-group-item.active a {
+        color: inherit;
+    }
+
+    .slide-enter {
+
+    }
+
+    .slide-enter-active {
+        animation: slideIn 0.5s;
+    }
+
+    .slide-enter-to {
+
+    }
+
+    .slide-leave {
+
+    }
+
+    .slide-leave-active {
+        animation: slideOut 0.5s;
+    }
+
+    .slide-leave-to {
+
+    }
+
+    @keyframes slideIn {
+        from {
+            transform: rotateY(90deg);
+        }
+        to {
+            transform: rotateY(0deg);
+        }
+    }
+
+    @keyframes slideOut {
+        from {
+            transform: rotateY(0deg);
+        }
+        to {
+            transform: rotateY(90deg);
+        }
+    }
 </style>
